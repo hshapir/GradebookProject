6a6b3e8bc302d49fd0b36c637cbd58d70429f935
Harrison Shapiro <hshapir@ucls.uchicago.edu>
2016-05-09 13:43:38 -0500

    Basic UI framework created

--- src/gradebookproject/UserInterfaceController.java
+++ src/gradebookproject/UserInterfaceController.java
@@ -10,7 +10,8 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableView;
 
 /**
  *
@@ -19,13 +20,12 @@ import javafx.scene.control.Label;
 public class UserInterfaceController implements Initializable {
     
     @FXML
-    private Label label;
+    private TableView gradebook;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    private MenuItem closeButton;
+    
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
