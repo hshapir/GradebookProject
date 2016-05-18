32ff7642bab823eb682ed865a5006f4f46bf0094
Harrison Shapiro <hshapir@ucls.uchicago.edu>
2016-05-18 13:27:33 -0500

    Add Assignment and Add Student buttons work

--- src/gradebookproject/UserInterfaceController.java
+++ src/gradebookproject/UserInterfaceController.java
@@ -14,6 +14,7 @@ import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableColumn;
@@ -148,7 +149,17 @@ public class UserInterfaceController implements Initializable {
         dialog.setContentText("Name:");
         Optional<String> result = dialog.showAndWait();
         if (result.isPresent()){
-            System.out.println("Your name: " + result.get());
+            if(currentSection.findStudent(result.get()) != null){
+                Alert invalidNameAlert = new Alert(Alert.AlertType.INFORMATION);
+                invalidNameAlert.setTitle("Invalid Student Name");
+                invalidNameAlert.setHeaderText(null);
+                invalidNameAlert.setContentText("You cannot have two students with the same name. Please enter an unused student name or add a '#1' or '#2'");
+                invalidNameAlert.showAndWait();
+                addStudent();
+            } else{
+                currentSection.addStudent(new Student(currentSection, result.get()));
+            }
+            updateTable();
         }
     }
     
@@ -193,7 +204,17 @@ public class UserInterfaceController implements Initializable {
         dialog.setContentText("New Assignment Name:");
         Optional<String> result = dialog.showAndWait();
         if (result.isPresent()){
-            System.out.println("Your name: " + result.get());
+            if(currentSection.findAssignment(result.get()) != null){
+                Alert invalidNameAlert = new Alert(Alert.AlertType.INFORMATION);
+                invalidNameAlert.setTitle("Invalid Assignment Name");
+                invalidNameAlert.setHeaderText(null);
+                invalidNameAlert.setContentText("You cannot have two assignments with the same name. Please enter an unused name or a date or number, like 'Test #2' or 'Homework 5/18'");
+                invalidNameAlert.showAndWait();
+                createAssignment();
+            } else{
+                currentSection.addAssignment(new Assignment(currentSection, result.get()));
+            }
+            updateTable();
         }
     }
     
